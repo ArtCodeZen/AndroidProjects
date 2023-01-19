a38a1bb78b3e711320079ee4dd241fba38a54243
artcodezen <henrique.watpequim@gmail.com>
2023-01-19 10:11:47 -0300

    Kotlin hello world

--- Kotlin/kotlinHelloWorld/app/src/main/java/com/example/kotlinhelloworld/MainActivity.kt
+++ Kotlin/kotlinHelloWorld/app/src/main/java/com/example/kotlinhelloworld/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.kotlinhelloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val text: TextView = findViewById(R.id.meu_te)
+        // toast
+        val duration = Toast.LENGTH_SHORT
+        val toast = Toast.makeText(applicationContext, text.text, duration)
+        toast.show()
     }
 }
