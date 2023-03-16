49a5d5e02d01bc2ec738a02418db5c75dd553aa0
artcodezen <henrique.watpequim@gmail.com>
2023-03-16 12:27:21 -0300

    add padding

--- Kotlin/HelloWorldComposable/app/src/main/java/com/example/helloworldcomposable/MainActivity.kt
+++ Kotlin/HelloWorldComposable/app/src/main/java/com/example/helloworldcomposable/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -12,6 +13,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.helloworldcomposable.ui.theme.HelloWorldComposableTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,7 +37,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String) {
 
     Surface(color = Color.Red){
-        Text(text = "Hello my name is: $name!")
+        Text(text = "Hello my name is: $name!", modifier = Modifier.padding(50.dp))
     }
 }
 
