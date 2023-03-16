26c2328a15baa561df0a30761be50e26c03835b7
artcodezen <henrique.watpequim@gmail.com>
2023-03-16 12:13:39 -0300

    Surface color

--- Kotlin/HelloWorldComposable/app/src/main/java/com/example/helloworldcomposable/MainActivity.kt
+++ Kotlin/HelloWorldComposable/app/src/main/java/com/example/helloworldcomposable/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.helloworldcomposable
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.helloworldcomposable.ui.theme.HelloWorldComposableTheme
 
@@ -31,7 +33,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello my name is: $name!")
+
+    Surface(color = Color.Red){
+        Text(text = "Hello my name is: $name!")
+    }
 }
 
 @Preview(showBackground = true)
