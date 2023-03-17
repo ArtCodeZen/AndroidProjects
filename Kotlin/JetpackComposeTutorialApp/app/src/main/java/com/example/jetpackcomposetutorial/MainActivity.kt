7f8ff6ab19234a9ab5a26156d7dd03cdd6b4bb4d
artcodezen <henrique.watpequim@gmail.com>
2023-03-17 17:45:10 -0300

    Jetpack Compose Tutorial

--- Kotlin/JetpackComposeTutorialApp/app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ Kotlin/JetpackComposeTutorialApp/app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.jetpackcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+
+
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposetutorial.ui.theme.JetPackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +38,43 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+@Composable
+fun myBackgroundImage(){
+    val image = painterResource(R.drawable.bg_compose_background)
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Fit
+        )
 
+
+}
+@Composable
+fun myText(){
+    Column{
+        myBackgroundImage()
+        Text(
+            text = stringResource(id = R.string.title),
+            fontSize = 24.sp,
+            modifier = Modifier.padding(16.dp),
+        )
+        Text(
+            text = stringResource(id = R.string.my_text_1),
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            textAlign = TextAlign.Justify
+        )
+        Text(
+            text = stringResource(id = R.string.my_text_2),
+            modifier = Modifier.padding(16.dp),
+            textAlign = TextAlign.Justify
+
+        )
+
+    }
+
+
+
+}
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
@@ -38,6 +84,7 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JetPackComposeTutorialTheme {
-        Greeting("Android")
+
+        myText()
     }
 }
