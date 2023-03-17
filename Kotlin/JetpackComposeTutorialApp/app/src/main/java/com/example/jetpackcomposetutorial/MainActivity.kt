8277d7fecbfbfa2cf28fa493d0e5bfc6f3d8a646
artcodezen <henrique.watpequim@gmail.com>
2023-03-17 10:40:36 -0300

    Adicionado a Imagem e adicionado o alinhamento do texto

--- Kotlin/JetpackComposeTutorialApp/app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ Kotlin/JetpackComposeTutorialApp/app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -1,59 +1,34 @@
-package com.example.myapplication
+package com.example.jetpackcomposetutorial
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.example.jetpackcomposetutorial.ui.theme.JetPackComposeTutorialTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            MyApplicationTheme {
+            JetPackComposeTutorialTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    BirthdayGreetingWithText(messageTxt = "Nova mensagem", from = "artcodezen")
+                    Greeting("Android")
                 }
             }
         }
     }
 }
 
-@Composable
-fun BirthdayGreetingWithText(messageTxt: String, from: String,modifier: Modifier = Modifier){
-    Column(modifier = modifier){
-        Text(
-            text = messageTxt,
-            color = Color.Red,
-            fontSize = 37.sp
-        )
-        Text(
-            text = from,
-
-            fontSize = 20.sp
-        )
-    }
-
-}
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
@@ -62,7 +37,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MyApplicationTheme {
-       BirthdayGreetingWithText("Mensagem ", "artcodezen")
+    JetPackComposeTutorialTheme {
+        Greeting("Android")
     }
 }
