a8b3b07976f9a53dc5eab1d23b3b61e84a17ee6a
artcodezen <henrique.watpequim@gmail.com>
2023-03-22 16:56:22 -0300

    Dado funcionando!

--- Kotlin/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ Kotlin/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,11 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -49,22 +46,37 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier
     .fillMaxHeight()
     .background(color = Color.White)
 ){
-    var result = 1
+    var result by remember {
+        mutableStateOf(1)
+    }
+    val imageResource = when(result){
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
     Column (
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ){
         Image(
-            painter = painterResource(id = R.drawable.dice_1),
+
+            painter = painterResource(id = imageResource),
             contentDescription = "1"
         )
-        Button(onClick = {
+        Button(
+
+            onClick = {
             result = (1..6).random()
         }) {
             Text(text = stringResource(id = R.string.roll))
-            Text(text = result.toString())
+
 
         }
+        Text(text = result.toString(),
+            color = Color.Black)
     }
 }
