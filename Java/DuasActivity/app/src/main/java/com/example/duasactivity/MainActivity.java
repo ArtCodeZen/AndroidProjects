3f9a9ce5ae0feba6793aa1fe8dcce9e170141a77
artcodezen <henrique.watpequim@gmail.com>
2024-06-27 22:37:55 -0300

    adicionado scrollview

--- Java/DuasActivity/app/src/main/java/com/example/duasactivity/MainActivity.java
+++ Java/DuasActivity/app/src/main/java/com/example/duasactivity/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.duasactivity;
 
+import android.content.pm.ActivityInfo;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -15,10 +17,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        //Rotacionar a tela
+        /*if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }*/
+
+        // utilizar esta orientação;
+        
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
