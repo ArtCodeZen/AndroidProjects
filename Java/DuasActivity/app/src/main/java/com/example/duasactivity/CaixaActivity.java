7ca490c281e87353a9181317f9f5fe16a24d3681
artcodezen <henrique.watpequim@gmail.com>
2024-07-23 09:47:05 -0300

    adicionado o eventListener

--- Java/DuasActivity/app/src/main/java/com/example/duasactivity/CaixaActivity.java
+++ Java/DuasActivity/app/src/main/java/com/example/duasactivity/CaixaActivity.java
@@ -1,15 +1,19 @@
 package com.example.duasactivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class CaixaActivity extends AppCompatActivity {
-
+    int contador = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,24 @@ public class CaixaActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button btn = findViewById(R.id.botaoSalvar);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                contador++;
+                Toast.makeText(getApplicationContext(), "Clicou: " + contador, Toast.LENGTH_SHORT).show();
+                /*Button myButton = (Button)view;
+                myButton.setText("OK");*/
+                ((Button) view).setText("Caixa");
+
+            }
+        });
+
     }
+/*    // função de clique - Modo normal de função
+    public void executar(View view){
+
+    }*/
 }
