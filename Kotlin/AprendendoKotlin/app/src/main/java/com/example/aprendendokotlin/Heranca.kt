19e9441025ce0e02513a6a89103b35b844599b29
Henrique Watanabe <henrique.watpequim@gmail.com>
2024-10-23 16:55:50 -0300

    adicionado exemplo de herança kotlin, em progresso...

---
+++ Kotlin/AprendendoKotlin/app/src/main/java/com/example/aprendendokotlin/Heranca.kt
@@ -0,0 +1,20 @@
+package com.example.aprendendokotlin
+
+class Cao {
+    var cor = ""
+    var tamanho = "" // pequeno, médio e grande porte
+    var peso = 20.6
+
+    fun correr(){
+        println("O cao está correndo")
+    }
+
+    fun dormir(){
+        println("O cao está dormindo")
+    }   
+}
+
+fun main(){
+    val nome:String = "artcodezen"
+
+}
--- Kotlin/AprendendoKotlin/app/src/main/java/com/example/aprendendokotlin/MetodoConstrutor.kt
+++ Kotlin/AprendendoKotlin/app/src/main/java/com/example/aprendendokotlin/MetodoConstrutor.kt
@@ -1,22 +1,25 @@
 package com.example.aprendendokotlin
 
 // classe com construtor primario
-class UsuarioComCosntrutor(var nome: String, var idade:Int) {
-
+class UsuarioComConstrutor(var nome: String, var idade:Int) {
+    /*
     init {
         this.nome = nome
         println("Objeto Usuario criado")
     }
-
+    */
     fun printNomeEIdade() {
         println("Nome: $nome Idade: $idade")
     }
 
     // construtor secundário sem definir o nome
-    constructor(nome: String) : this(nome, 0) {
+    constructor(nome: String) : this(nome, idade = 0) {
     }
 }
 fun main(){
-    var user = UsuarioComCosntrutor("artcodezen", 27)
+    var user = UsuarioComConstrutor("artcodezen", 27)
     user.printNomeEIdade()
+    var user2 = UsuarioComConstrutor("Maria", 30)
+    user2.printNomeEIdade()
+
 }
