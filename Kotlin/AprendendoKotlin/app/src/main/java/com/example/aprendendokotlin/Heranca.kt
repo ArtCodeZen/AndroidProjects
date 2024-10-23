d21bb16e8c47ce862858b93a4c92ddf35ce4f12b
Henrique Watanabe <henrique.watpequim@gmail.com>
2024-10-23 18:32:47 -0300

    herança exemplo finalizado classes métodos e chamada super

--- Kotlin/AprendendoKotlin/app/src/main/java/com/example/aprendendokotlin/Heranca.kt
+++ Kotlin/AprendendoKotlin/app/src/main/java/com/example/aprendendokotlin/Heranca.kt
@@ -1,20 +1,37 @@
 package com.example.aprendendokotlin
-
-class Cao {
+open class Animal{      // open o mesmo que public em java
     var cor = ""
     var tamanho = "" // pequeno, médio e grande porte
-    var peso = 20.6
+    var peso = 0
+    open fun dormir(){
+        print("O animal ")
+    }
+}
+class Cao : Animal(){
 
     fun correr(){
-        println("O cao está correndo")
+        println("cao ${cor} está correndo ")
     }
-
-    fun dormir(){
-        println("O cao está dormindo")
+    // override funciona como o @Override do java sobreescrever um metodo
+    override fun dormir(){
+        super.dormir()
+        println("cao está dormindo")
     }   
 }
+class Passaro : Animal(){
 
+
+    fun voar(){
+        println("passaro ${cor} está voando")
+    }
+
+
+}
 fun main(){
     val nome:String = "artcodezen"
-
+    val cao = Cao()
+    cao.cor = "Marrom"
+    cao.dormir()
+    val passaro = Passaro()
+    passaro.dormir()
 }
