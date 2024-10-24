f50b128fa16d3b662dae54e00486a28d76f96d6e
Henrique Watanabe <henrique.watpequim@gmail.com>
2024-10-24 10:25:10 -0300

    usando a interface

--- Kotlin/AprendendoKotlin/app/src/main/java/com/example/aprendendokotlin/ModificadoresDeAcesso.kt
+++ Kotlin/AprendendoKotlin/app/src/main/java/com/example/aprendendokotlin/ModificadoresDeAcesso.kt
@@ -1,6 +1,10 @@
 package com.example.aprendendokotlin
+// interface cria um contrato que as classes que implementam devem seguir
 
-open class Computador{
+interface firewall{
+    fun bloquear(ip:String)
+}
+open class Computador : firewall{
     private var modelo = ""
 
     fun ligar(){
@@ -15,10 +19,15 @@ open class Computador{
     fun getModelo():String{
         return this.modelo
     }
+
+    override fun bloquear(ip: String) {
+        println("Bloqueando o ip $ip")
+    }
 }
 
 fun main(){
     var pc = Computador()
     pc.ligar()
+    pc.bloquear("192.168.0.1")
     
 }
