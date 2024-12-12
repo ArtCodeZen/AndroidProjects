323c4790acca57b2c906987d8c0b86a4b6629acb
Henrique Watanabe <henrique.watpequim@gmail.com>
2024-12-11 21:10:51 -0300

    pair or triple

---
+++ Kotlin/AulaComponentesListagemColecoes/app/src/main/java/com/example/aulacomponenteslistagemcolecoes/PairTriple.kt
@@ -0,0 +1,12 @@
+package com.example.aulacomponenteslistagemcolecoes
+
+fun main(){
+    //val localizacao = Pair(10,20)
+    //val localizacao = 10 to 20
+
+    val localizacao = Triple(10,20,30)
+    println(localizacao.first)
+    println(localizacao.second)
+    println(localizacao.third)
+
+}
