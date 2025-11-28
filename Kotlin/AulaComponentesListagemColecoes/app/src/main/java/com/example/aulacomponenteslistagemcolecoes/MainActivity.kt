a45f43e97bfcbee8af986bd33ede0f290e04186f
Henrique Watanabe <henrique.watpequim@gmail.com>
2025-11-28 20:55:43 -0300

    novos atualizações

--- Kotlin/AulaComponentesListagemColecoes/app/src/main/java/com/example/aulacomponenteslistagemcolecoes/MainActivity.kt
+++ Kotlin/AulaComponentesListagemColecoes/app/src/main/java/com/example/aulacomponenteslistagemcolecoes/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.example.aulacomponenteslistagemcolecoes
 
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var btnExecutar: Button
-    private lateinit var textResultado: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,11 +15,6 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        btnExecutar = findViewById(R.id.btn_executar)
-        textResultado = findViewById(R.id.text_resultado)
-        btnExecutar.setOnClickListener {
-            textResultado.text = "Olá, Mundo!"
         }
     }
 }
