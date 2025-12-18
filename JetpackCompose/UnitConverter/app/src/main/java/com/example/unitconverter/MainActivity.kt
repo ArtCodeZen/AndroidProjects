074a2a075d8e7170d2bc554f540e474e8ea9e70f
Henrique Watanabe <henrique.watpequim@gmail.com>
2025-12-17 21:30:00 -0300

    Adicionado input box e colunas

--- JetpackCompose/UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ JetpackCompose/UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
@@ -31,26 +36,30 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun UnitConverter(modifier: Modifier = Modifier){
     Column(modifier){
-        Greeting("Android", modifier)
-        Greeting("Android", modifier)
+        Text("Unit Converter")
+        OutlinedTextField(value = "Digite algo", onValueChange = {/* here goes what should happen, when the Value of our OutlinedTextField changes}*/})
         Row{
-            Greeting("Linha", modifier)
+            val context = LocalContext.current
+
+            Button(onClick = { Toast.
+                makeText(context,
+                    "Button Clicked",
+                    Toast.LENGTH_SHORT).show()}){
+                Text("Clique aqui")
+            }
+            Box(){
+
+            }
         }
 
+
     }
-}
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun UnitConverterPreview() {
     UnitConverterTheme {
-        Greeting("Android")
+        UnitConverter()
     }
 }
