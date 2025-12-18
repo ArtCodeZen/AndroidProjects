5079ae52fe7a8bc99f5783bcd2677d1a191086a8
Henrique Watanabe <henrique.watpequim@gmail.com>
2025-12-18 19:03:33 -0300

    Adicionado o offset, para criar espaçamento entre o dropdown menu

--- JetpackCompose/UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ JetpackCompose/UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -15,10 +15,14 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -29,6 +33,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
@@ -54,7 +59,7 @@ fun UnitConverter(modifier: Modifier){
         Spacer(modifier = Modifier.height(30.dp))       // espaçamento
         OutlinedTextField(value = "Digite algo", onValueChange = {/* here goes what should happen, when the Value of our OutlinedTextField changes}*/})
         Spacer(modifier = Modifier.height(30.dp))       // espaçamento
-        Row{
+        Row(horizontalArrangement = Arrangement.spacedBy(10.dp)){
             val context = LocalContext.current
 
             Button(onClick = { Toast.
@@ -63,16 +68,47 @@ fun UnitConverter(modifier: Modifier){
                     Toast.LENGTH_SHORT).show()}){
                 Text("Clique aqui")
             }
+
             Box(){
+
                 Button(onClick = {}){
                     Text("Select")
                     Icon(Icons.Default.ArrowDropDown,
                         contentDescription = "Arrow Down")
 
                 }
-            }
-            Box(){
 
+                DropdownMenu(expanded = true, onDismissRequest = {}, offset = DpOffset(0.dp, 20.dp) ) {
+                    DropdownMenuItem(
+                        text = {Text("Centimeters")},
+                        onClick = {})
+                    DropdownMenuItem(
+                        text = {Text("Feet")},
+                        onClick = {})
+                    DropdownMenuItem(
+                        text = {Text("Milimeters")},
+                        onClick = {})
+                }
+            }
+
+            Box(){
+                Button(onClick = {}){
+                    Text("Select1")
+                    Icon(Icons.Default.ArrowDropDown,
+                        contentDescription = "Arrow Down")
+
+                }
+                DropdownMenu(expanded = false, onDismissRequest = {}) {
+                    DropdownMenuItem(
+                        text = {Text("Centimeters")},
+                        onClick = {})
+                    DropdownMenuItem(
+                        text = {Text("Feet")},
+                        onClick = {})
+                    DropdownMenuItem(
+                        text = {Text("Milimeters")},
+                        onClick = {})
+                }
             }
         } // row
 
