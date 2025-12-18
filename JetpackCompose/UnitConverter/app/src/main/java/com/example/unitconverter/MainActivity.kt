b52a464fe68c89f63ebab0c763b185fd51a65cdc
Henrique Watanabe <henrique.watpequim@gmail.com>
2025-12-18 10:03:21 -0300

    modifier fill max size

--- JetpackCompose/UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ JetpackCompose/UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -5,11 +5,16 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
@@ -21,8 +26,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,19 +39,21 @@ class MainActivity : ComponentActivity() {
         setContent {
             UnitConverterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                   UnitConverter(modifier = Modifier.padding(innerPadding))
+                   UnitConverter(modifier = Modifier.padding(innerPadding).fillMaxSize())
                 }
             }
         }
     }
 }
 @Composable
-fun UnitConverter(modifier: Modifier = Modifier){
+fun UnitConverter(modifier: Modifier){
     Column(modifier,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally){
-        Text("Unit Converter")
+        Text("Unit Converter", modifier= Modifier.border(BorderStroke(2.dp, Color.Black)).padding(2.dp))
+        Spacer(modifier = Modifier.height(30.dp))       // espaçamento
         OutlinedTextField(value = "Digite algo", onValueChange = {/* here goes what should happen, when the Value of our OutlinedTextField changes}*/})
+        Spacer(modifier = Modifier.height(30.dp))       // espaçamento
         Row{
             val context = LocalContext.current
 
@@ -75,6 +84,6 @@ fun UnitConverter(modifier: Modifier = Modifier){
 @Composable
 fun UnitConverterPreview() {
     UnitConverterTheme {
-        UnitConverter()
+        UnitConverter(Modifier.fillMaxSize())
     }
 }
