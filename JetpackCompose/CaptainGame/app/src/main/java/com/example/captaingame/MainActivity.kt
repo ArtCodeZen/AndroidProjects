433c93ab9651eead9d6b97f922364497d973d99b
Henrique Watanabe <henrique.watpequim@gmail.com>
2025-12-19 10:05:00 -0300

    trocado para o uso do by no mutable state of

--- JetpackCompose/CaptainGame/app/src/main/java/com/example/captaingame/MainActivity.kt
+++ JetpackCompose/CaptainGame/app/src/main/java/com/example/captaingame/MainActivity.kt
@@ -14,8 +14,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,58 +41,61 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CaptainGame(modifier: Modifier){
-    val treasuresFound = remember {mutableStateOf(0)}
-    val direction = remember { mutableStateOf("North") }
-    val stormIndicator = remember { mutableStateOf("Normal") }
+    /*Para remover o uso do valor.value
+    * Usamos o by remember { mutableStateOf("value")}
+    * */
+    var treasuresFound by remember {mutableStateOf(0)}
+    var direction by remember { mutableStateOf("North") }
+    var stormIndicator by remember { mutableStateOf("Normal") }
     Column(modifier, horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-        Text(text = "Treasures found: ${treasuresFound.value}" )
-        Text(text = "Current Direction: ${direction.value}")
-        Text(text = "${stormIndicator.value}")
+        Text(text = "Treasures found: ${treasuresFound}" )
+        Text(text = "Current Direction: ${direction}")
+        Text(text = "${stormIndicator}")
         Spacer(modifier = Modifier.height(15.0.dp))
         // East
         Button(onClick = {
-            direction.value = "East"
+            direction = "East"
             if(Random.nextBoolean()){
-                treasuresFound.value++
-                stormIndicator.value = "Found a treasure"
+                treasuresFound++
+                stormIndicator = "Found a treasure"
             }else{
-                stormIndicator.value = "Storm"
+                stormIndicator = "Storm"
             }
         }) {
             Text(text = "Sail East")
         }
         // West
         Button(onClick = {
-            direction.value = "West"
+            direction = "West"
             if(Random.nextBoolean()){
-                treasuresFound.value++
-                stormIndicator.value = "Found a treasure"
+                treasuresFound++
+                stormIndicator = "Found a treasure"
             }else{
-                stormIndicator.value = "Storm"
+                stormIndicator = "Storm"
             }
         }) {
             Text(text = "Sail West")
         }
         // North
         Button(onClick = {
-            direction.value = "North"
+            direction = "North"
             if(Random.nextBoolean()){
-                treasuresFound.value++
-                stormIndicator.value = "Found a treasure"
+                treasuresFound++
+                stormIndicator = "Found a treasure"
             }else{
-                stormIndicator.value = "Storm"
+                stormIndicator = "Storm"
             }
         }) {
             Text(text = "Sail North")
         }
         // South
         Button(onClick = {
-            direction.value = "South"
+            direction = "South"
             if(Random.nextBoolean()){
-                treasuresFound.value++
-                stormIndicator.value = "Found a treasure"
+                treasuresFound++
+                stormIndicator = "Found a treasure"
             }else{
-                stormIndicator.value = "Storm"
+                stormIndicator = "Storm"
             }
         }) {
             Text(text = "Sail South")
