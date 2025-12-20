d719b14e68d7fc3de6f58490749db277f5ba6314
Henrique Watanabe <henrique.watpequim@gmail.com>
2025-12-19 22:46:49 -0300

    Conversora funcionando!

--- JetpackCompose/UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ JetpackCompose/UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -1,8 +1,6 @@
 package com.example.unitconverter
 
-import android.R
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,17 +9,13 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -38,11 +32,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
+import kotlin.math.roundToInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -65,8 +59,20 @@ fun UnitConverter(modifier: Modifier){
     var outputUnit by remember { mutableStateOf("Meters") }
     var iExpanded by remember { mutableStateOf(false) }
     var oExpanded by remember { mutableStateOf(false) }
-    val conversionFactor by remember { mutableStateOf(0.01) }
+    val oConversionFactor = remember { mutableStateOf(1.00) }
+    val conversionFactor = remember { mutableStateOf(1.00) }
+    fun convertUnit(){
+        // elvis operator ?:
+        val inputValueDouble = inputValue.toDoubleOrNull()?:0.0
+        // 1. Faz a conversão pura
+        val rawResult = inputValueDouble * conversionFactor.value / oConversionFactor.value
 
+        // 2. Arredonda para 2 casas decimais
+        val result = (rawResult * 100).roundToInt() / 100.0
+
+        outputValue = result.toString()
+
+    }
     Column(modifier,
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally){
@@ -74,20 +80,21 @@ fun UnitConverter(modifier: Modifier){
         Spacer(modifier = Modifier.height(30.dp))       // espaçamento
 
         OutlinedTextField(colors = OutlinedTextFieldDefaults.colors(focusedTextColor = Color.Black,
-            focusedBorderColor = Color.Black,
+
             unfocusedTextColor = Color.Red),value = inputValue, onValueChange = {
-            inputValue = it})
+                inputValue = it
+                convertUnit()
+            },
+            label = {Text("Enter value")})
 
         Spacer(modifier = Modifier.height(30.dp))       // espaçamento
         Row(horizontalArrangement = Arrangement.spacedBy(10.dp)){
-            val context = LocalContext.current
-            Box(){
-
+            Box{
                 Button(onClick = {
                     iExpanded = true
 
                 }){
-                    Text("Select")
+                    Text(inputUnit)
                     Icon(Icons.Default.ArrowDropDown,
                         contentDescription = "Arrow Down")
 
@@ -96,22 +103,48 @@ fun UnitConverter(modifier: Modifier){
                 DropdownMenu(expanded = iExpanded, onDismissRequest = {  iExpanded = false}, offset = DpOffset(0.dp, 20.dp) ) {
                     DropdownMenuItem(
                         text = {Text("Centimeters")},
-                        onClick = {})
+                        onClick = {
+                            iExpanded = false
+                            inputUnit = "Centimeters"
+                            conversionFactor.value = 0.01
+                            convertUnit()
+
+                        })
+                    DropdownMenuItem(
+                        text = {Text("Meters")},
+                        onClick = {
+                            iExpanded = false
+                            inputUnit = "Meters"
+                            conversionFactor.value = 1.00
+                            convertUnit()
+
+                        })
                     DropdownMenuItem(
                         text = {Text("Feet")},
-                        onClick = {})
+                        onClick = {
+                            iExpanded = false
+                            inputUnit = "Feet"
+                            conversionFactor.value = 0.3048
+                            convertUnit()
+                        })
                     DropdownMenuItem(
                         text = {Text("Milimeters")},
-                        onClick = {})
+                        onClick = {
+                            iExpanded = false
+                            inputUnit = "Milimeters"
+                            // milimeters
+                            conversionFactor.value = 0.001
+                            convertUnit()
+                        })
                 }
             }
 
-            Box(){
+            Box{
                 Button(onClick = {
                     oExpanded = true
 
                 }){
-                    Text("Select")
+                    Text(outputUnit)
                     Icon(Icons.Default.ArrowDropDown,
                         contentDescription = "Arrow Down")
 
@@ -120,17 +153,41 @@ fun UnitConverter(modifier: Modifier){
                 DropdownMenu(expanded = oExpanded, onDismissRequest = {  oExpanded = false}, offset = DpOffset(0.dp, 20.dp) ) {
                     DropdownMenuItem(
                         text = {Text("Centimeters")},
-                        onClick = {})
+                        onClick = {
+                            oExpanded = false
+                            outputUnit = "Centimeters"
+                            oConversionFactor.value = 0.01
+                            convertUnit()
+                        })
+                    DropdownMenuItem(
+                        text = {Text("Meters")},
+                        onClick = {
+                            oExpanded = false
+                            outputUnit = "Meters"
+                            oConversionFactor.value = 1.00
+                            convertUnit()
+
+                        })
                     DropdownMenuItem(
                         text = {Text("Feet")},
-                        onClick = {})
+                        onClick = {
+                            oExpanded = false
+                            outputUnit = "Feet"
+                            oConversionFactor.value = 0.3048
+                            convertUnit()
+                        })
                     DropdownMenuItem(
                         text = {Text("Milimeters")},
-                        onClick = {})
+                        onClick = {
+                            oExpanded = false
+                            outputUnit = "Milimeters"
+                            oConversionFactor.value = 0.001
+                            convertUnit()
+                        })
                 }
             }
         } // row
-        Spacer(modifier = Modifier.height(30.dp))       // espaçamento)
+        Spacer(modifier = Modifier.height(30.dp))       // espaçamento
         Text("Result: $outputValue" )
 
 
